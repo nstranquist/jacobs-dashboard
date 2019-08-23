893c57d0fff89218c4448cb842fb99d5b695a5d8
nicostran <nstranquist@gmail.com>
2019-08-22 19:34:25 -0500

    add parcel-bundler to make into webapp for dashboard content section

---
+++ components/action-1.js
@@ -0,0 +1,6 @@
+const action1Template = `
+  <h1>Hello Action 1</h1>
+  <p>Description</p>
+`;
+
+render(action1Template, document.getDocumentById('#content'));
---
+++ index.js
@@ -0,0 +1,8 @@
+// import modules
+import notes from './components/notes';
+
+
+// render function
+const render = function (template) {
+  document.getElementById('content').innerHTML = template;
+}
